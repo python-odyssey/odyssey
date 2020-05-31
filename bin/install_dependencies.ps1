fc0c5d4b3f0677f78e4e37ff82160e39fb63bb00
Justin Sharma <justin.elite@gmail.com>
2020-05-30 22:10:11 -0700

    Fix AppVeyor.

--- bin/install_dependencies.ps1
+++ bin/install_dependencies.ps1
@@ -1,2 +1,6 @@
+if (!(Test-Path $Env:PYTHON_EXECUTABLE)) {
+    throw "Env:PYTHON_EXECUTABLE is missing!"
+}
+
 poetry env use $Env:PYTHON_EXECUTABLE;
 poetry install;
--- bin/run_tox.ps1
+++ bin/run_tox.ps1
@@ -1,2 +1,7 @@
+if (!(Test-Path $Env:PYTHON_EXECUTABLE)) {
+    throw "Env:PYTHON_EXECUTABLE is missing!"
+}
+
+poetry env use $Env:PYTHON_EXECUTABLE;
 poetry run pip --disable-pip-version-check install poetry
 poetry run tox

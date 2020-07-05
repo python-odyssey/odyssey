948329f5838acf784d0270b66423129142e2c5f8
Justin Sharma <justin.elite@gmail.com>
2020-07-05 00:14:09 -0700

    build: Make get-poetry executable after downloading.

--- bin/install_poetry.sh
+++ bin/install_poetry.sh
@@ -8,4 +8,4 @@ else
   PYTHON="${PYTHON_EXECUTABLE}"
 fi
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py && $PYTHON_EXECUTABLE ./get-poetry.py --yes && rm -f ./get-poetry.py
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py && chmod u+x ./get-poetry.py && $PYTHON_EXECUTABLE ./get-poetry.py --yes && rm -f ./get-poetry.py

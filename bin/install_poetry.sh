2f0cb16bbb23cd619002ce8334ad50d86f7258ca
Justin Sharma <justin.elite@gmail.com>
2020-09-16 22:07:04 -0700

    chore: We now pin poetry versions in the scripts.

--- bin/install_poetry.sh
+++ bin/install_poetry.sh
@@ -8,4 +8,4 @@ else
   PYTHON_EXECUTABLE="${PYTHON_EXECUTABLE}"
 fi
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py && chmod u+x ./get-poetry.py && $PYTHON_EXECUTABLE ./get-poetry.py --yes && rm -f ./get-poetry.py
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -o get-poetry.py && chmod u+x ./get-poetry.py && $PYTHON_EXECUTABLE ./get-poetry.py --yes --version 1.0.10 && rm -f ./get-poetry.py

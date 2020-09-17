01b99d5c1a0b1973697cb36c0f88b71627633bf1
Justin Sharma <justin.elite@gmail.com>
2020-09-16 22:14:58 -0700

    build: Roll poetry version back one to avoid bug.

--- bin/install_poetry.ps1
+++ bin/install_poetry.ps1
@@ -2,4 +2,4 @@ if (!(Test-Path $Env:PYTHON_EXECUTABLE)) {
     throw "Env:PYTHON_EXECUTABLE is missing!"
 }
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing -OutFile get-poetry.py; & $Env:PYTHON_EXECUTABLE get-poetry.py --yes --version 1.0.10; Remove-Item get-poetry.py;
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing -OutFile get-poetry.py; & $Env:PYTHON_EXECUTABLE get-poetry.py --yes --version 1.0.9; Remove-Item get-poetry.py;

836c727c0fbcf556ccad74d2f3afeb9089261d9a
Justin Sharma <justin.elite@gmail.com>
2020-11-14 21:37:00 -0800

    chore: Start work on categorizing callable types.

--- src/odyssey/reflect.py
+++ src/odyssey/reflect.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import inspect
+from enum import Enum, unique
 
 def is_callable(obj) -> bool:
     return callable(obj)
+
+class CallableType(Enum):
+    Unknown = 0
+    FreeFunction = 1
+
+def callable_type():
+    pass
--- src/odyssey_tests/reflect_tests.py
+++ src/odyssey_tests/reflect_tests.py
@@ -3,6 +3,21 @@ from odyssey.reflect import is_callable
 def free_function():
     pass
 
+class empty_class():
+    pass
+
+class example_class():
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+    
+    def member_function(self):
+        pass
+
+example_object = example_class()
+
 def test_is_callable():
     assert is_callable(int)
     assert is_callable(str)
@@ -12,3 +27,9 @@ def test_is_callable():
     assert not is_callable(["hello", "world"])
     assert is_callable(free_function)
     assert not is_callable(free_function())
+    assert is_callable(empty_class)
+    assert is_callable(example_class)
+    assert is_callable(example_class())
+    assert is_callable(example_class.member_function)
+    assert is_callable(example_class().member_function)
+    assert is_callable(example_object)

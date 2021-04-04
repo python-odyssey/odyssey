4217f87b03c2362cd34ee4ee12e92fd485aea76b
Justin Sharma <justin.elite@gmail.com>
2021-04-04 16:20:07 -0700

    chore: Fix syntax error in activate_env.

--- bin/activate_env.ps1
+++ bin/activate_env.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [String]
-    $Python = (if(Test-Path "Env:PYTHON_EXECUTABLE") { "$Env:PYTHON_EXECUTABLE" } else { "python" })
+    $Python = $(if(Test-Path "Env:PYTHON_EXECUTABLE") { "$Env:PYTHON_EXECUTABLE" } else { "python" })
 )
 
 poetry env use $Python
